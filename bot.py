37d330d5d2af9a7466d07f9c8a07fa2790da5016
Chris Colomb <hellokurisu.io@gmail.com>
2024-05-29 11:21:59 +0900

    Bot quickstart

---
+++ bot.py
@@ -0,0 +1,16 @@
+import nextcord
+from nextcord.ext import commands
+
+GUILD_ID = 1244859066472468552
+
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(description="My first slash command", guild_ids=[GUILD_ID])
+async def hello(interaction: nextcord.Interaction):
+    await interaction.send("Hello!")
+
+bot.run('MTI0NDg3MjU2MTQ5MDI2ODE2MQ.G4PF_b.NywvLd3lR1tSY-8EMpF_ECDKrlzIluiBBK86lk')
