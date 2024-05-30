070fce5e53b39d17bc339c2ac436ce8011124eef
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2024-05-30 02:21:51 -0700

    deleted testing.js

--- testing.js
+++
@@ -1,9 +0,0 @@
-import Api from './js/api-service.js';
-
-
-const neo_address = 'NYtBFomNFzMPsKosGajLaJ7NoaQ1b7cZXj';
-
-const api = new Api(neo_address);
-
-const pool = await api.getPool(neo_address);
-console.log(pool);
